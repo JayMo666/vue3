3811b16020706441a4921abea4552f18512c7902
JayMo666 <mojuegui@gmail.com>
2021-01-11 22:26:32 +0800

    主题分离2

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
-const isLib = process.env.npm_lifecycle_script.includes('--type lib')
+const isLib =
+  process.env &&
+  process.env.npm_lifecycle_script &&
+  process.env.npm_lifecycle_script.includes('--type lib')
 module.exports = {
   configureWebpack(config) {},
   chainWebpack(config) {
-    // console.log('isLib', isLib)
     if (!isLib) {
       config.plugin('monaco').use(MonacoWebpackPlugin)
       config.plugin('circular').use(CircularDependencyPlugin)
